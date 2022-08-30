a3b708a3fc3ce3fe66b9fe69bf626c9547d45b0e
khobin <gatobin24@naver.com>
2022-08-30 16:08:31 +0900

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by 김호빈 on 2022/08/30.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
